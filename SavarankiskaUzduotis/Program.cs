9318c76068d26aee89da37ffec616ff18e0a427f
Dovydoff <70907949+Dovydoff@users.noreply.github.com>
2022-10-19 19:18:33 +0300

    Testas

--- SavarankiskaUzduotis/Program.cs
+++ SavarankiskaUzduotis/Program.cs
@@ -1,4 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-//Test
+//Test asda
+
+float zemesSpindulys = 6371;
+float pi = 3.14f;
+float pavirsiausPlotas = zemesSpindulys * zemesSpindulys * pi;
+Console.WriteLine(pavirsiausPlotas);
+
